ec04b1b5a412fa9f222f1946c39e50d4d0e3f9ca
Hamzeh Khazaei <hkh@yorku.ca>
2025-06-19 15:53:06 -0400

    Create testgetcourseno.c

---
+++ source/testgetcourseno.c
@@ -0,0 +1,9 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(void)
+{
+    printf(1, "Note: The OS course number at York University is: %d\n", getcourseno());
+    exit();
+}
